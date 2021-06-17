8be78b3a6d633a5afb59db1849101003722d7588
Zaid <me@zaid-arshad.tech>
2021-06-17 07:51:20 -0400

    Update post-docs.js

--- scripts/post-docs.js
+++ scripts/post-docs.js
@@ -2,6 +2,6 @@ const { writeFile } = require("fs/promises");
 const { join } = require("path");
 
 (async () => {
-    await writeFile(join(__dirname, "..", "docs", "CNAME"), "zaida04.github.io");
+    // await writeFile(join(__dirname, "..", "docs", "CNAME"), "zaida04.github.io");
     console.log("CREATED CNAME");
 })();
